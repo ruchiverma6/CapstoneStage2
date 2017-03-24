3c22dcf56a5dffb8c2f835efb771c5f33526183f
Ruchi Devi (Cognizant Technology Solutions) <v-ruchd@microsoft.com>
2017-03-24 15:45:32 +0530

    added changes

--- app/src/main/java/com/example/v_ruchd/capstonestage2/HomeActivity.java
+++ app/src/main/java/com/example/v_ruchd/capstonestage2/HomeActivity.java
@@ -1,20 +1,40 @@
 package com.example.v_ruchd.capstonestage2;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.v_ruchd.capstonestage2.adapters.ChatListAdapter;
+
 public class HomeActivity extends AppCompatActivity {
+private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private ChatListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setUpActionBar();
+        initComponents();
+    }
+
+    private void initComponents() {
+        mRecyclerView=(RecyclerView)findViewById(R.id.recyler_view);
+
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new ChatListAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     /***
