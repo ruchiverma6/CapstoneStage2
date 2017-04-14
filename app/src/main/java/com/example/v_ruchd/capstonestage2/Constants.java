23c48ff1c4a8829b339b5943fd3ba07b8c14420c
ruchi <ruchiverma6@gmail.com>
2017-04-14 21:10:41 +0530

    added changes

--- app/src/main/java/com/example/v_ruchd/capstonestage2/Constants.java
+++ app/src/main/java/com/example/v_ruchd/capstonestage2/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
     public static String LUIS_APP_KEY = "94b64dd1-f8de-4f99-8a44-9a2c677397b8";
 
 
-    public static final String NEWS_OPEN_API_KEY = "f165ebb5933b4880aa1aca93c75e96de";
+    public static final String NEWS_OPEN_API_KEY = "";
 
 }
